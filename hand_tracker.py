7c14d7f2ab68a1f231296bd21217e107b1655528
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-03 22:13:41 -0300

    Inicializando testes profissionais

--- hand_tracker.py
+++ hand_tracker.py
@@ -16,8 +16,33 @@ class Detector:
                  model_complexity: int = 1,
                  min_detection_confidence: confidence = 0.5,
                  min_tracking_confidence: confidence = 0.5):
+        #Parametros necessarios para inicializar o hands
         self.mode = mode
         self.max_number_hands = number_hands
         self.complexity = model_complexity
         self.detection_con = min_detection_confidence
         self.tracking_con = min_tracking_confidence
+
+        # Inicializar o Hands
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(self.mode,
+                                         self.max_number_hands,
+                                         self.complexity,
+                                         self.detection_con,
+                                         self.tracking_con)
+        self.tip_ids = [4, 8, 12, 16, 20]
+
+
+# Teste de classe ====================
+if __name__ == "__main__":
+    Detect = Detector()
+
+    capture = cv2.VideoCapture(0)
+
+    while True:
+        _, img = capture.read()
+
+        cv2.imshow("Image", img)
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
